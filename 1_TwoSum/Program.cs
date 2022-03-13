c8b281d803bd48913bd0f88c1845d1e481f143ae
PuFGGs <dorukakin35@hotmail.com>
2022-03-13 02:12:01 +0000

    Initial commit

---
+++ 1_TwoSum/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1_TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Solution solution = new Solution();
+
+            foreach (var item in solution.TwoSum(new int[] { 3, 2, 3 }, 6))
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ 3_LongestSubstringWithoutRepeatingCharacters/Solution.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _3_LongestSubstringWithoutRepeatingCharacters
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            List<char> temp = new List<char>();
+
+            int longestFound = 0;
+            int found = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+
+                if (temp.Contains(c))
+                {
+                    int index = temp.FindIndex(x => x.Equals(c)) + 1;
+
+                    temp = temp.Where(x => temp.FindIndex(y => y.Equals(x)) > index - 1).ToList();
+                }
+                temp.Add(c);
+
+                found = temp.Count;
+                longestFound = Math.Max(found, longestFound);
+            }
+
+
+            if (longestFound == 0)
+                longestFound = temp.Count;
+
+            return Math.Max(found, longestFound);
+        }
+    }
+}
