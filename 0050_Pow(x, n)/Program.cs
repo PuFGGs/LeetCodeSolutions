3a29fac74bdbd46cfd4c539e0a5deeba4b0d00e6
PuFGGs <dorukakin35@hotmail.com>
2023-08-05 20:49:49 +0100

    .

---
+++ 0050_Pow(x, n)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _0050_Pow_x__n_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 0050_Pow(x, n)/Solution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0050_Pow_x__n_
+{
+    public class Solution
+    {
+        public double MyPow(double x, int n) => Math.Pow(x, n);
+    }
+}
